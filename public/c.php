75b6d2184b62b4281b49254541934f52eb2b410d
CamdynR <rasque.camdyn@gmail.com>
2022-05-30 20:29:59 -0700

    Updated color php thing

--- public/c.php
+++ public/c.php
@@ -1,15 +1,18 @@
+<?php
+$path = $_SERVER["REQUEST_URI"];
+$color = explode("/", $path)[2];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <?php
+    print_r("<title>#{$color}</title>");
+    print_r("<style>html,body{background-color:#${color}}</style>");
+  ?>
 </head>
 <body>
-<?php
-  $path = $_SERVER["DOCUMENT_ROOT"];
-  $color = explode("/", $path);
-  print_r($color);
-?>
 </body>
 </html>
